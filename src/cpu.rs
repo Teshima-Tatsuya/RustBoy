48e07761eb41d06d46f0fddded6f8c38e0227ece
Tatsuya Teshima <terima666tatsuya@gmail.com>
2022-05-08 18:17:01 +0900

    inst

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,4 @@
+use crate::opcode::*;
 use crate::types::*;
 use bitvec::prelude::*;
 
@@ -92,6 +93,8 @@ impl Cpu {
     pub fn new() -> Self {
         Self::default()
     }
+
+    pub fn step() {}
 }
 
 #[cfg(test)]
