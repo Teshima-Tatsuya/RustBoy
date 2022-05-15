24137321933d54ce82ca0c6d82553809893b3a77
Tatsuya Teshima <terima666tatsuya@gmail.com>
2022-05-15 14:32:03 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use rust_boy::cpu::*;
 use rust_boy::rom::*;
+use rust_boy::types::*;
 use std::env;
 use std::fs::File;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let file = File::open(&args[0]);
+    let bytes = std::fs::read(&args[0]).unwrap();
 
-    let rom = Rom::new(file);
+    let rom = Rom::new(&bytes);
 
     let cpu = Cpu::new();
     cpu.reg.F.z;
--- src/rom.rs
+++ src/rom.rs
@@ -1,4 +1,7 @@
 use crate::types::*;
+use anyhow::{bail, Result};
+use std::fs::File;
+use std::io::{BufReader, Read};
 
 #[derive(Default)]
 pub struct Rom {
@@ -6,9 +9,9 @@ pub struct Rom {
     logo: [Byte; 30],
     /// 0x013F-0x0142 Manufacturer Code
     /// 0x0143        CGB Flag
-    title: [Byte; 10],
-    new_licence_code: [Byte; 2],
-    sgb_flag: [Byte; 2],
+    title: String,
+    new_licensee_code: [Byte; 2],
+    sgb_flag: bool,
     cartridge_type: Byte,
     rom_size: Byte,
     ram_size: Byte,
@@ -21,8 +24,57 @@ pub struct Rom {
     data: Vec<Byte>,
 }
 
+#[derive(Default, Clone)]
+struct CartridgeType {
+    code: Byte,
+    mbc: Mbc,
+    has_ram: bool,
+    has_battery: bool,
+    has_timer: bool,
+    has_runble: bool,
+    has_sensor: bool,
+}
+
+impl CartridgeType {
+    fn new(code: Byte) -> Self {
+        let ct = Default::default();
+
+        ct
+    }
+}
+
+#[derive(Clone)]
+enum Mbc {
+    NoMbc,
+    Mbc1,
+    Mbc2,
+    Mbc3,
+    Mbc4,
+    Mbc5,
+    Mbc6,
+    Mbc7,
+    HuC1,
+    Mmm01,
+}
+
 impl Rom {
-    pub fn new(buf: Vec<Byte>) -> Self {
-        Self::default()
+    pub fn new(buf: &[Byte]) -> Result<Self> {
+        let entry_point = buf[0x100..=0x103].try_into()?;
+        let logo = buf[0x104..=0x133].try_into()?;
+        let title = String::from_utf8_lossy(&buf[0x13f..=0x142]).to_string();
+        let new_licensee_code = buf[0x144..=0x145].try_into()?;
+        let sgb_flag = match buf[0x146] {
+            0x00 => false,
+            0x03 => true,
+            v => bail!("Invalid SGB flag: ${v:02X}"),
+        };
+
+        Ok(Rom {
+            entry_point,
+            logo,
+            title,
+            new_licensee_code,
+            sgb_flag,
+        })
     }
 }
