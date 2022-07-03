f86bd50854d98b694f8ea918c31f9e27a811f6e0
Tatsuya Teshima <terima666tatsuya@gmail.com>
2022-07-03 17:50:58 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,11 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    if args.len() == 1 {
+        println!("Please input rom file path as args 1");
+        return;
+    }
+
     let bytes = std::fs::read(&args[1]).unwrap();
 
     let wraped_cart = Cartridge::new(&bytes);
