0c75e8b06db39924a42224c60a9a738617cb11c5
Tatsuya Teshima <terima666tatsuya@gmail.com>
2022-08-02 22:55:27 +0900

    add vram

--- src/bus.rs
+++ src/bus.rs
@@ -8,6 +8,7 @@ use crate::{
 
 pub struct Bus {
     mbc: Mbc,
+    vram: RAM,
     wram: RAM,
     wram2: RAM,
     hram: RAM,
@@ -18,6 +19,7 @@ impl Bus {
     pub fn new(mbc: Mbc) -> Self {
         Bus {
             mbc,
+            vram: RAM::new(0x2000),
             wram: RAM::new(0x4000),
             wram2: RAM::new(0x4000),
             hram: RAM::new(0x0080),
@@ -30,6 +32,7 @@ impl Reader for Bus {
     fn read(&self, addr: Word) -> Byte {
         match addr {
             0x0000..=0x7FFF => self.mbc.read(addr),
+            0x8000..=0x9FFF => self.vram.read(addr),
             0xC000..=0xDFFF => self.wram.read(addr - 0xC000),
             0xE000..=0xFDFF => self.wram2.read(addr - 0xE000),
             0xFF01..=0xFF70 | 0xFFFF => self.io.read(addr),
@@ -43,6 +46,7 @@ impl Writer for Bus {
     fn write(&mut self, addr: Word, value: Byte) {
         match addr {
             0x0000..=0x7FFF => self.mbc.write(addr, value),
+            0x8000..=0x9FFF => self.vram.write(addr, value),
             0xC000..=0xDFFF => self.wram.write(addr - 0xC000, value),
             0xE000..=0xFDFF => self.wram2.write(addr - 0xE000, value),
             0xFF01..=0xFF70 | 0xFFFF => self.io.write(addr, value),
