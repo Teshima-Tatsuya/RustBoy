bb9d928c314b55cae90e722e38260c6f238ac715
Tatsuya Teshima <terima666tatsuya@gmail.com>
2022-08-02 22:56:34 +0900

    fix bus

--- src/bus.rs
+++ src/bus.rs
@@ -32,7 +32,7 @@ impl Reader for Bus {
     fn read(&self, addr: Word) -> Byte {
         match addr {
             0x0000..=0x7FFF => self.mbc.read(addr),
-            0x8000..=0x9FFF => self.vram.read(addr),
+            0x8000..=0x9FFF => self.vram.read(addr - 0x8000),
             0xC000..=0xDFFF => self.wram.read(addr - 0xC000),
             0xE000..=0xFDFF => self.wram2.read(addr - 0xE000),
             0xFF01..=0xFF70 | 0xFFFF => self.io.read(addr),
@@ -46,7 +46,7 @@ impl Writer for Bus {
     fn write(&mut self, addr: Word, value: Byte) {
         match addr {
             0x0000..=0x7FFF => self.mbc.write(addr, value),
-            0x8000..=0x9FFF => self.vram.write(addr, value),
+            0x8000..=0x9FFF => self.vram.write(addr - 0x8000, value),
             0xC000..=0xDFFF => self.wram.write(addr - 0xC000, value),
             0xE000..=0xFDFF => self.wram2.write(addr - 0xE000, value),
             0xFF01..=0xFF70 | 0xFFFF => self.io.write(addr, value),
