036e94507d4847e7582e8e393bb53372248458c6
Tatsuya Teshima <terima666tatsuya@gmail.com>
2022-08-20 16:56:41 +0900

    add bevy_pixels

--- src/emulator.rs
+++ src/emulator.rs
@@ -1,10 +1,6 @@
-use crate::{
-    gameboy::GameBoy,
-    constant::*,
-};
-use bevy::{
-    prelude::*,
-};
+use crate::{constant::*, gameboy::GameBoy};
+use bevy::{prelude::*, window::WindowResizeConstraints};
+use bevy_pixels::prelude::*;
 
 pub struct Emulator {
     pub gb: GameBoy,
@@ -12,21 +8,34 @@ pub struct Emulator {
 
 impl Emulator {
     pub fn new(gb: GameBoy) -> Self {
-        Self {
-            gb: gb,
-        }
+        Self { gb: gb }
     }
 
     pub fn init(&self) {
         App::new()
-        .insert_resource(WindowDescriptor {
-            title: "rustboy".to_string(),
-            width: SCREEN_WIDTH as f32,
-            height: SCREEN_HEIGHT as f32,
-            ..Default::default()
-        })
-        .add_plugins(DefaultPlugins)
-        .add_system(bevy::input::system::exit_on_esc_system)
-        .run();
+            .insert_resource(WindowDescriptor {
+                title: "rustboy".to_string(),
+                width: SCREEN_WIDTH as f32,
+                height: SCREEN_HEIGHT as f32,
+                resize_constraints: WindowResizeConstraints {
+                    min_width: SCREEN_WIDTH as f32,
+                    min_height: SCREEN_HEIGHT as f32,
+                    ..default()
+                },
+                ..default()
+            })
+            .insert_resource(PixelsOptions {
+                width: SCREEN_WIDTH as u32,
+                height: SCREEN_HEIGHT as u32,
+            })
+            .add_plugins(DefaultPlugins)
+            .add_plugin(PixelsPlugin)
+            .add_system(bevy::input::system::exit_on_esc_system)
+            .add_system(main_system)
+            .run();
     }
+}
+
+fn main_system(mut pixels_resource: ResMut<PixelsResource>) {
+    let frame: &mut [u8] = pixels_resource.pixels.get_frame();
 }
